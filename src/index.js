30b99843a9e317f0f379b72e044c6041981bc9a4
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2020-11-23 19:17:31 -0500

    Add Zendesk base CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@zendeskgarden/css-bedrock';
 
 const mountNode = document.getElementById('app');
 ReactDOM.render(<App name="Steve"/>, mountNode);
