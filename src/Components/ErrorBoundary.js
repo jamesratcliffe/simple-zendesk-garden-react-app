7cc077a77c593a12d0327047b5a02637211676e6
James Ratcliffe <james.ratcliffe@lightspeedhq.com>
2021-09-10 10:37:47 -0400

    Show fallback message in error boundary

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import { notify } from '../lib/Zendesk';
 
+/**
+ * The error boundary will catch errors that happen in the app.
+ */
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = {hasError: false};
+    this.state = { hasError: false };
   }
 
+  /**
+   * Handle the error by updating the state and sending a Zendesk notification.
+   *
+   * @param error
+   * @param errorInfo
+   */
   componentDidCatch(error, errorInfo) {
+    this.setState({ hasError: true });
     notify(
         `Error in Sync Toolbox app: ${error.message}`,
         'error',
-        true
+        true,
     );
-
   }
 
   render() {
+    // This is what to render when there was an error
     if (this.state.hasError) {
-      location.reload();
-      return null;
+      return <p>Couldn't load app.</p>;
     }
 
     return this.props.children;
